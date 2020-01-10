85396c46d52af320a1c93d09bbe63bd3fdd83920
gnaze <gnazeola@gmail.com>
2020-01-10 12:42:09 +0100

    lesson 4

---
+++ app/src/main/java/com/example/swoosh/SkillActivity.kt
@@ -0,0 +1,19 @@
+package com.example.swoosh
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class SkillActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_skill)
+    }
+
+   fun leagueNextClicked(view: View){
+    val skillActivity = Intent(this, SkillActivity::class.java)
+       startActivity(skillActivity)
+}
+}
